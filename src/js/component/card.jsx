d98b588b844e9e65bfe464575b6b1fd010e5b1e8
NVR-2023 <nunoviolanterodrigues@hotmail.com>
2023-04-04 12:39:43 +0000

    Current version 04-04-2023 Provisional <Card /> component

---
+++ src/js/component/card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = (props) => {
+
+    return (
+        <div className="card" style={{width: '18rem'}}>
+          <img src={props.imageUrl} alt={props.cardTitle} />
+          <div className="card-body">
+            <h5 className="card-title">{props.cardTitle}</h5>
+            <p className="card-text">{props.cardDescription}</p>
+            <a href={props.buttonUrl} className="btn btn-primary">{props.buttonLabel}</a>
+          </div>
+        </div>
+    );
+};
+
+export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,73 @@
 import React from "react";
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import image1 from "../../img/01.jpg";
+import image2 from "../../img/02.jpg";
+import image3 from "../../img/03.jpg";
+import image4 from "../../img/04.jpg";
 
-//create your first component
+const cardSet = [
+  {
+    imageUrl: image1,
+    cardTitle: "Title 1",
+    cardDescription: "This is a description",
+    buttonUrl: "https://4geeksacademy.com/",
+    buttonLabel: "4Geeks",
+  },
+  {
+    imageUrl: image2,
+    cardTitle: "Title 2",
+    cardDescription: "This is a description",
+    buttonUrl: "https://4geeksacademy.com/",
+    buttonLabel: "4Geeks",
+  },
+  {
+    imageUrl: image3,
+    cardTitle: "Title 3",
+    cardDescription: "This is a description",
+    buttonUrl: "https://4geeksacademy.com/",
+    buttonLabel: "4Geeks",
+  },
+  {
+    imageUrl: image4,
+    cardTitle: "Title 4",
+    cardDescription: "This is a description",
+    buttonUrl: "https://4geeksacademy.com/",
+    buttonLabel: "4Geeks",
+  },
+];
+
+const jsxCardElement = cardSet.map((object, index) => {
+  return (
+    <div className="col-sm-12 col-md-3 py-2" key={index}>
+      <Card
+        imageUrl={object.imageUrl}
+        cardTitle={object.cardTitle}
+        cardDescription={object.cardDescription}
+        buttonUrl={object.buttonUrl}
+        buttonLabel={object.buttonLabel}
+      />
+    </div>
+  );
+});
+
+//H Home component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div>
+      <div className="gx-0">
+        <Navbar />
+      </div>
+      <div className="container-fluid m-3 gx-0">
+        <div>
+          <Jumbotron />
+        </div>
+        <div className="container-fluxi d-flex gx-0 row">{jsxCardElement}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
