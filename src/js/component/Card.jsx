83be56024430c92d0ebefed421afe2c2024d3101
CabraCrimes <benalexanderthegreat@gmail.com>
2023-04-05 09:28:03 +0000

    Cards v2

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -4,8 +4,8 @@ import React from "react";
 export const Card = (props) => {
     
 return(
-    <div className="card text-center"  style={{width: "18rem"}}>
-        <img src={props.imageUrl} className="card-img-top" alt="Image"/>
+    <div className="card text-center" style={{width: "18rem"}} >
+        <img src={props.imageUrl} className="card-img-top" style={{height: "16rem"}} alt="Image"/>
         <div className="card-body">
             <h5 className="card-title">{props.cardTitle}</h5>
             <p className="card-text">{props.cardDescription}</p>
